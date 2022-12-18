07ba6236e64a03e9866a34b5720bc922c85d8944
Paola Becerra <paonata_marquez@outlook.com>
2022-12-18 14:43:26 -0500

    add viewset for students

--- projects/api.py
+++ projects/api.py
@@ -1,9 +1,16 @@
 from projects.models import Project
+from projects.models import Student
 from rest_framework import viewsets, permissions
 from .serializers import ProjectSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ProjectSerializer
