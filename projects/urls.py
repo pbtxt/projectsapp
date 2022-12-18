225ed88410737346076e4628fea57ebafb7211a0
Paola Becerra <paonata_marquez@outlook.com>
2022-12-18 14:43:40 -0500

    add urls for students crud

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 from .api import ProjectViewSet
+from .api import StudentViewSet
 
 router = routers.DefaultRouter()
 
 router.register('api/projects', ProjectViewSet, 'projects')
+router.register('api/students', StudentViewSet, 'students')
 
 urlpatterns = router.urls
