d6f9e39a39748c73a9577daba406a09093c3d32b
Paola Becerra <paonata_marquez@outlook.com>
2022-12-18 14:59:08 -0500

    add grades viewset

--- projects/api.py
+++ projects/api.py
@@ -1,8 +1,10 @@
 from projects.models import Project
 from projects.models import Student
+from projects.models import Grade
 from rest_framework import viewsets, permissions
 from .serializers import ProjectSerializer
 from .serializers import StudentSerializer
+from .serializers import GradeSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -15,3 +17,9 @@ class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = StudentSerializer
+
+
+class GradeViewSet(viewsets.ModelViewSet):
+    queryset = Grade.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = GradeSerializer
