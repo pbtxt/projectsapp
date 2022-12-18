aae59b86541c5f522b5fe8e67506e1e33fe896cc
Paola Becerra <paonata_marquez@outlook.com>
2022-12-18 14:42:07 -0500

    add new model for students table

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,10 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     technology = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Student(models.Model):
+    internalId = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    careerName = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
