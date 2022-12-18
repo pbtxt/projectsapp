c98a10727bd6414f164ef8b890d56921467b418f
Paola Becerra <paonata_marquez@outlook.com>
2022-12-18 14:58:34 -0500

    add grades model serializers

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Project
 from .models import Student
+from .models import Grade
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -13,5 +14,12 @@ class ProjectSerializer(serializers.ModelSerializer):
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
+        fields = '__all__'
+        read_only_fields = ('created_at',)
+
+
+class GradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grade
         fields = '__all__'
         read_only_fields = ('created_at',)
