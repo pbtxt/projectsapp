2b9051410f069378739fad6b12d23943966e6fb1
Paola Becerra <paonata_marquez@outlook.com>
2022-12-18 14:43:06 -0500

    add serializer for students model

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from .models import Project
+from .models import Student
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
+        fields = '__all__'
+        read_only_fields = ('created_at',)
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
         fields = '__all__'
         read_only_fields = ('created_at',)
