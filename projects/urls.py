405d39a71bd7a8cf2505ac3d69b22d8c6d2f4315
Paola Becerra <paonata_marquez@outlook.com>
2022-12-18 14:59:20 -0500

    add grades urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
 from .api import ProjectViewSet
 from .api import StudentViewSet
+from .api import GradeViewSet
 
 router = routers.DefaultRouter()
 
 router.register('api/projects', ProjectViewSet, 'projects')
 router.register('api/students', StudentViewSet, 'students')
+router.register('api/grades', GradeViewSet, 'grades')
 
 urlpatterns = router.urls
